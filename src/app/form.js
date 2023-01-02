afef08eb042864f0461b786a38d77dbc4da4bf98
Y0ni0 <eshcolsam@gmail.com>
2023-01-02 22:43:56 +0300

    nested form started

---
+++ src/app/form.js
@@ -0,0 +1,9 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+import React from 'react'
+
+export default function form() {
+    return (
+        <div>Grupos / Slots</div>
+    )
+}
